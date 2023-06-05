d359a7178f776d04650338900b4b092850504232
junhyeonp <psjking1229@naver.com>
2023-06-05 15:32:48 +0900

    good

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,12 +1,38 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Button, Input, Radio, RadioGroup, Stack, Text, VStack } from "@chakra-ui/react";
 import Layout from "./Layout";
+import { useState } from "react";
 
 export default function Contact() {
+  const [name, setName] = useState("");
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
   return (
     <Layout>
-      <Box w="inherit">
-        <Text>연락처</Text>
-      </Box>
+      <VStack as={"form"} w="full" bg="gray.400" minH="100vh" py="140px" alignItems={"flex-start"} px={4} spacing={6}>
+        <Text fontWeight="700" fontSize={20}>
+          무료상담신청
+        </Text>
+        <VStack w="full" alignItems={"flex-start"}>
+          <Text fontWeight="600">Name</Text>
+          <Input onChange={handleChange} value={name} bg="gray.100" placeholder="이름을 입력해 주세요"></Input>
+        </VStack>
+        <VStack w="full" alignItems={"flex-start"}>
+          <Text fontWeight="600">연락처</Text>
+          <Input bg="gray.100" placeholder="연락처를 입력해 주세요"></Input>
+        </VStack>
+        <VStack w="full" alignItems={"flex-start"}>
+          <Text fontWeight="600">email</Text>
+          <Input bg="gray.100" placeholder="이메일을 입력해 주세요"></Input>
+        </VStack>
+        <RadioGroup>
+          <Stack direction="row">
+            <Radio value="1">남성</Radio>
+            <Radio value="2">여성</Radio>
+          </Stack>
+        </RadioGroup>
+        <Button colorScheme="blue">전송하기</Button>
+      </VStack>
     </Layout>
   );
 }
--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,13 +1,25 @@
-import { Box, Text, VStack } from "@chakra-ui/react";
+import { Box, Input, Text, VStack } from "@chakra-ui/react";
 import Layout from "./Layout";
 
 export default function Profile() {
   return (
     <Layout>
-      <VStack py="140px" alignItems={"flex-start"} px={4}>
+      <VStack py="140px" alignItems={"flex-start"} px={4} spacing={6}>
         <Text fontWeight={700} fontSize={20}>
           무료상담신청
         </Text>
+        <VStack w="full" alignItems={"flex-start"}>
+          <Text>이름</Text>
+          <Input placeholder="이름을 입력해 주세요"></Input>
+        </VStack>
+        <VStack w="full" alignItems={"flex-start"}>
+          <Text>연락처</Text>
+          <Input placeholder="연락처를 입력해 주세요"></Input>
+        </VStack>
+        <VStack w="full" alignItems={"flex-start"}>
+          <Text>email</Text>
+          <Input placeholder="이메일을 입력해 주세요"></Input>
+        </VStack>
       </VStack>
     </Layout>
   );
